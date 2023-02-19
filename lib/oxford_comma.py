1dd66dc799b85b2710db6854181efcf58f00f803
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 05:00:47 -0500

    use join() to join items in the list

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
+# convert items in a string which is separated by ", "
 def oxford_comma(items):
-    return None
+    length = len(items)
+    if length == 1:
+        return items[0]
+    elif length == 2:
+        return " and ".join(items)
+    else:
+        if length >= 3:
+            return ", and ".join([", ".join(item for item in items[0: -1]), items[-1]])
